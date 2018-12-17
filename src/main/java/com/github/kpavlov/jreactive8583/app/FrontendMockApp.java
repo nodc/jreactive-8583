8592e039a01ca4fc908f67716e13bef2587d1aad
Sukhonosov <Sukhonosov@bpcbt.com>
2018-12-17 18:23:12 +0300

    added spring boot

---
+++ src/main/java/com/github/kpavlov/jreactive8583/app/FrontendMockApp.java
@@ -0,0 +1,13 @@
+package com.github.kpavlov.jreactive8583.app;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class FrontendMockApp {
+
+    public static void main(String[] args) {
+        run(FrontendMockApp.class, args);
+    }
+}
